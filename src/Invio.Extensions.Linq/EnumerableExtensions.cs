f635b4ae8bc99f09f5a7ab76d772a92bf3e0c5cd
Brian Caruso <carusobp@gmail.com>
2018-01-25 12:53:13 -0800

    Add cycling IEnumerable / IEnumerable<T> implementations

---
+++ src/Invio.Extensions.Linq/EnumerableExtensions.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Invio.Extensions.Linq {
+
+    /// <summary>
+    ///   Broad concepts that extend the <see cref="IEnumerable" /> and
+    ///   <see cref="IEnumerable{T}" /> functionality that is currently
+    ///   available within the System.* namespaces.
+    /// </summary>
+    public static class EnumerableExtensions {
+
+        /// <summary>
+        ///   Similar to <see cref="Enumerable.AsEnumerable" />, this
+        ///   method returns an <see cref="IEnumerable{T}" /> that contains
+        ///   all of the items in <paramref name="source" />, but instead of
+        ///   stopping at the end of the original enumerable, it returns
+        ///   once again to the beginning.
+        /// </summary>
+        /// <remarks>
+        ///   The order in which the items will be returned will always
+        ///   be the order in which they currently exist within
+        ///   <paramref name="source" />, with the first item immediately
+        ///   following the last item.
+        /// </remarks>
+        /// <param name="source">
+        ///   The <see cref="IEnumerable{T}" /> that will be able to be
+        ///   enumerated ad infinitum.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   Lazily thrown when <paramref name="source" /> is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///   Lazily thrown when <paramref name="source" /> is empty.
+        /// </exception>
+        /// <returns>
+        ///   An <see cref="IEnumerable{T}" /> which is able to enumerate
+        ///   over all items in <paramref name="source" /> ad infinitum.
+        /// </returns>
+        public static IEnumerable<T> Cycle<T>(this IEnumerable<T> source) {
+            if (source == null) {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            var enumerator = source.GetEnumerator();
+
+            while (true) {
+                if (!enumerator.MoveNext()) {
+                    enumerator = source.GetEnumerator();
+
+                    if (!enumerator.MoveNext()) {
+                        throw new ArgumentException(
+                            $"The enumerable provided is empty.",
+                            nameof(source)
+                        );
+                    }
+                }
+
+                yield return enumerator.Current;
+            }
+        }
+
+        /// <summary>
+        ///   Similar to <see cref="Enumerable.AsEnumerable" />, this
+        ///   method returns an <see cref="IEnumerable" /> that contains
+        ///   all of the items in <paramref name="source" />, but instead of
+        ///   stopping at the end of the original enumerable, it returns
+        ///   once again to the beginning.
+        /// </summary>
+        /// <remarks>
+        ///   The order in which the items will be returned will always
+        ///   be the order in which they currently exist within
+        ///   <paramref name="source" />, with the first item immediately
+        ///   following the last item.
+        /// </remarks>
+        /// <param name="source">
+        ///   The <see cref="IEnumerable" /> that will be able to be
+        ///   enumerated ad infinitum.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   Lazily thrown when <paramref name="source" /> is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///   Lazily thrown when <paramref name="source" /> is empty.
+        /// </exception>
+        /// <returns>
+        ///   An <see cref="IEnumerable" /> which is able to enumerate
+        ///   over all items in <paramref name="source" /> ad infinitum.
+        /// </returns>
+        public static IEnumerable Cycle(this IEnumerable source) {
+            if (source == null) {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            var enumerator = source.GetEnumerator();
+
+            while (true) {
+                if (!enumerator.MoveNext()) {
+                    enumerator = source.GetEnumerator();
+
+                    if (!enumerator.MoveNext()) {
+                        throw new ArgumentException(
+                            $"The enumerable provided is empty.",
+                            nameof(source)
+                        );
+                    }
+                }
+
+                yield return enumerator.Current;
+            }
+        }
+
+    }
+
+}
