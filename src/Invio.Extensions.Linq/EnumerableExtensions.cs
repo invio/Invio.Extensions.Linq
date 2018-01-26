5cdaf58667b397ab108d8fa04adba81a1630c5cd
Brian Caruso <carusobp@gmail.com>
2018-01-25 18:17:10 -0800

    Add Zip() implementation that defaults to tuples

--- src/Invio.Extensions.Linq/EnumerableExtensions.cs
+++ src/Invio.Extensions.Linq/EnumerableExtensions.cs
@@ -112,6 +112,47 @@ namespace Invio.Extensions.Linq {
             }
         }
 
+        /// <summary>
+        ///   An overload of <see cref="Enumerable.Zip" />, this method
+        ///   combines the corresponding elements of two enumerables into
+        ///   a single sequence of tuples.
+        /// </summary>
+        /// <remarks>
+        ///   The method merges each element of the first sequence with
+        ///   an element that has the same index in the second sequence.
+        ///   If the sequences do not have the same number of elements,
+        ///   the method merges sequences until it reaches the end of
+        ///   one of them. For example, if one sequence has three elements
+        ///   and the other one has four, the result sequence will have
+        ///   only three elements.
+        /// </remarks>
+        /// <typeparam name="TFirst">
+        ///   The type of the elements of the first input sequence.
+        /// </typeparam>
+        /// <typeparam name="TSecond">
+        ///   The type of the elements of the second input sequence.
+        /// </typeparam>
+        /// <param name="first">
+        ///   The first enumerable to merge.
+        /// </param>
+        /// <param name="second">
+        ///   The second enumerable to merge.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   Thrown when <paramref name="first" /> or <paramref name="second" />
+        ///   is null.
+        /// </exception>
+        /// <returns>
+        ///   An final enumerable that combines the corresponding elements
+        ///   of the two input sequences into a sequence of tuples.
+        /// </returns>
+        public static IEnumerable<Tuple<TFirst, TSecond>> Zip<TFirst, TSecond>(
+            this IEnumerable<TFirst> first,
+            IEnumerable<TSecond> second) {
+
+            return first.Zip(second, Tuple.Create);
+        }
+
     }
 
 }
